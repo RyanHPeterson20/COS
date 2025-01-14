95307551dad11bc05528203d3f826ac84733c77e
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-01-13 21:08:39 -0700

    Add files via upload

--- bspline_poly.c
+++ bspline_poly.c
@@ -1,13 +1,17 @@
 #include <R.h>
 #include <Rinternals.h>
+#include <math.h>
 
-//test section for some of the internal functions needed.
-#include <stdio.h>
-
-
-
-// comment
-void cubicSplineBasis(){
+// comment 
+// testing power functions
+void cubicSplineBasis(double *x, double *result){
   
+  double term1 = pow(fmax(*x + 2, 0), 3);
+  double term2 = 4 * pow(fmax(*x + 1, 0), 3);
+  double term3 = 6 * pow(fmax(*x, 0), 3);
+  double term4 = 4 * pow(fmax(*x - 1, 0), 3);
+  double term5 = pow(fmax(*x - 2, 0), 3);
+  
+  *result = (1.0 / 6.0) * (term1 - term2 + term3 - term4 + term5);
 }
 
