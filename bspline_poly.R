1a60a614b9a562bd2628ab76a0847be16458a4e1
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-01-21 16:09:11 -0700

    Add files via upload

--- bspline_poly.R
+++ bspline_poly.R
@@ -1,7 +1,20 @@
 
 #bspline polynomial function
 
-cubicSplineBasis <- function(x){
-  return((1/6) *(pmax(x + 2, 0)^3 - 4 * (pmax(x + 1, 0)^3) + 6 * (pmax(x, 0)^3) -
-                   4 * (pmax(x - 1, 0)^3) + pmax(x - 2, 0)^3))  
+#TODO: add in some checks for outside of support so we don't have to evaluate everything
+#TODO: convert to C (check which runs faster)
+
+#compare pmax and max
+#cubicSplineBasis <- function(x){
+#  return((1/6) *(pmax(x + 2, 0)^3 - 4 * (pmax(x + 1, 0)^3) + 6 * (pmax(x, 0)^3) -
+#                   4 * (pmax(x - 1, 0)^3) + pmax(x - 2, 0)^3))  
+#}
+
+#testing variation and new name
+CubicBSpline <- function(d){
+  d <- d*2 #added in to deal with 
+  return((1/6) *(pmax(d + 2, 0)^3 - 4 * (pmax(d + 1, 0)^3) + 6 * (pmax(d, 0)^3) -
+                   4 * (pmax(d - 1, 0)^3) + pmax(d - 2, 0)^3) * (abs(d) < 2))  
 }
+
+
